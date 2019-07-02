dc6c74db501e799a0571a200b672642674d90fcd
Frédéric Wang <fwang@igalia.com>
2019-07-02 15:14:07 +0200

    Add tests for text-transform and fix links for CSS tests.

--- extract-wpt-tests/extract.sh
+++ extract-wpt-tests/extract.sh
@@ -7,7 +7,7 @@ fi
 
 MATHML_CORE_URL=https://mathml-refresh.github.io/mathml-core
 WPT_DIR=$1
-WPT_SUBDIRS="mathml css/css-fonts/math-script-level-and-math-style" # css/css-text/text-transform/math
+WPT_SUBDIRS="mathml css/css-fonts/math-script-level-and-math-style css/css-text/text-transform/math"
 PYTHON_SCRIPT=wpt-tests-generator.py
 EXTRACTED_TESTS=wpt-tests.txt
 
@@ -16,6 +16,7 @@ echo "
 # This file was automatically generatd from extract.sh. Do not edit.
 
 from __future__ import print_function
+import re
 
 tests = {}
 def add_test(section, path):
@@ -26,13 +27,20 @@ def add_test(section, path):
 def format_tests():
     for section in sorted(tests):
         print('$MATHML_CORE_URL/#%s' % section)
-        print('data-tests=\"%s\"\n' % ','.join(sorted(tests[section])))
+        test_list=[]
+        for test in sorted(tests[section]):
+            result = re.match( r'mathml/(.*)', test)
+            if result:
+                test_list.append(result.group(1))
+            else:
+                test_list.append('../%s' % test)
+        print('data-tests=\"%s\"\n' % ','.join(test_list))
 
 " > $PYTHON_SCRIPT
 
 for subdir in $WPT_SUBDIRS; do
     echo -n "Extracting tests from $WPT_DIR/$subdir... "
-    find $WPT_DIR/$subdir -type f -name '*.html' | xargs grep '<link' | grep 'rel="help"' | grep $MATHML_CORE_URL | sed "s|$WPT_DIR/$subdir/\([^:]\+\):.\+$MATHML_CORE_URL/#\([^\"]\+\)\".\+|add_test('\2','\1')|" >> $PYTHON_SCRIPT
+    find $WPT_DIR/$subdir -type f -name '*.html' | xargs grep '<link' | grep 'rel="help"' | grep $MATHML_CORE_URL | sed "s|$WPT_DIR/\($subdir/[^:]\+\):.\+$MATHML_CORE_URL/#\([^\"]\+\)\".\+|add_test('\2','\1')|" >> $PYTHON_SCRIPT
     echo "done"
 done
 
