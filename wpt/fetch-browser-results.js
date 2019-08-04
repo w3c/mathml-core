3876419bd9e7a4ac27bf1791d9398d9c4680e1a5
Frédéric Wang <fwang@igalia.com>
2019-08-04 11:10:18 +0200

    Escape test names in implementation report.

--- wpt/fetch-browser-results.js
+++ wpt/fetch-browser-results.js
@@ -5,3 +5,9 @@ async function fetchWebPlatformTestResults(browser) {
     let response = await fetch(`https://build-chromium.igalia.com/mathml/wpt/${browser}-latest.json`);
     return response.json();
 }
+
+function escapeHTML(source) {
+    return source.replace(/[&<>"'/]/g, c => {
+        return `&#x${c.charCodeAt(0).toString(16)};`;
+    });
+}
