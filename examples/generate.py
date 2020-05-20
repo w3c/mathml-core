39630b3057626d32a52e44923729e3be157d3bbb
Frédéric Wang <fwang@igalia.com>
2020-05-20 13:51:27 +0200

    Add script to generate examples and add one for mfrac.

---
+++ examples/generate.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from urllib.parse import quote
+import glob
+import os
+import re
+import subprocess
+import sys
+
+CHROMIUM_PATH = "chromium-mathml"
+IMAGE_MAGICK = "convert"
+
+def generateScreenShot(name):
+    example = open(name, "rb")
+    url = "file://%s/container.html?mathBase64=%s" % (
+        os.path.dirname(os.path.realpath(sys.argv[0])),
+        quote(example.read())
+    )
+    image = name.replace(".html", ".png")
+    subprocess.run([CHROMIUM_PATH,
+                    "--headless",
+                    "--disable-gpu",
+                    "--screenshot=%s" % image,
+                    "--enable-blink-features=MathMLCore",
+                    "--window-size=500x500",
+                    url])
+    components = subprocess.check_output([IMAGE_MAGICK,
+                                          image,
+                                          "-define", "connected-components:verbose=true",
+                                          "-connected-components", "4",
+                                          "-auto-level",
+                                          "out.png"]).decode('ascii')
+    os.remove("out.png")
+    for line in components.splitlines():
+        match = re.match("^\s+0: ([0-9]+x[0-9]+\+0\+0)", str(line))
+        if match:
+            subprocess.run([IMAGE_MAGICK,
+                            image,
+                            "-transparent", "white",
+                            "-crop", match.groups()[0],
+                            image])
+            break
+     
+
+# Generate screenshot for the specified file example, or for all of them.
+if len(sys.argv) >= 2:
+    generateScreenShot(sys.argv[1])
+else:
+    for name in glob.glob("example-*.html"):
+        generateScreenShot(name)
