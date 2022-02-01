59ec075b42bfc64b6693fdfc3bd24734d3867cdf
Frédéric Wang <fwang@igalia.com>
2022-02-01 13:24:10 +0100

    Rewrite section B.1 Operator Dictionary...
    ... and the algorithm for determining the properties of an embellished
    operator. There should be no behavior change. However section B.1 now
    describes two algorithms:
    
    - to determine the category of an operator
    - to set the properties of an operator from its category
    
    which are used by the algorithm to set the properties of an embellished
    operator. This makes it more convenient for implementations and will
    allow to work around the issue with multiple forms tried on operators
    that can be both in the Default category for the infix form and in a
    different category for a non-infix form.
    
    https://github.com/w3c/mathml-core/issues/104

--- tables/operator-dictionary.py
+++ tables/operator-dictionary.py
@@ -636,6 +636,7 @@ value_index = 0
 md.write('<figure id="operator-dictionary-categories-values">')
 md.write("<table>");
 md.write("<tr><th>Category</th><th>Form</th><th>Encoding</th><th>rspace</th><th>lspace</th><th>properties</th></tr>\n")
+md.write("<tr><td>Default</td><td>N/A</td><td>N/A</td><td><code>0.2777777777777778em</code></td><td><code>0.2777777777777778em</code></td><td>N/A</td></tr>\n");
 for name, item in sorted(knownTables.items(),
                          key=(lambda v: len(v[1]["singleChar"])),
                          reverse=True):
